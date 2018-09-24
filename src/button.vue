ae4beb115d8da9322bf52971318561bfaa45e7b1
JackWong992 <jackwong992@163.com>
2018-09-24 16:19:53 +0800

    添加icon和iconPosition

---
+++ src/button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon">
+            <use  v-bind:xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+<script>
+    export  default  {
+        props:['icon','iconPosition']
+    }
+</script>
+
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        vertical-align: middle;//解决inline-flex带来的按钮不对齐
+        justify-content: center;
+        align-items: center;
+        &:hover {border-color: var(--border-color-hover);}
+        &:active {background-color: var(--button-active-bg);}
+        &:focus {outline: none;}
+        > .content{ order: 2; }
+        > .icon{ order: 1;margin-right: .1em;}
+        &.icon-right{
+            > .content{order: 1;}
+            >.icon{order: 2; margin-left:.1em;margin-right: 0;}
+        }
+    }
+
+
+</style>
